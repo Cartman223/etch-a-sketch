addcc3317e4c34f950dffd91b2150fe1719472cf
Cartman223 <jpirmao37@gmail.com>
2024-05-22 16:53:05 -0300

    add button prompt to get how many squares should the grid have

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // get a reference to the container div
 const board = document.getElementById('board');
 // set maximum size of the sketch pad(pixels)
-const boardMaxSize = 920;
+const boardMaxHeight = 600;
+const boardMaxWidth = 600;
 
 // create rows
-const rowSize = 16;
+let rowSize = 16;
 for(r = 0; r < rowSize; r++) {
     let row = document.createElement('div');
     row.classList.toggle('row');
@@ -19,12 +20,11 @@ for(r = 0; r < rowSize; r++) {
     // dynamically alter the size of squares to fit the dimensions of the board
     const cell = document.getElementsByClassName('white-square');
     for(k = 0; k < cell.length; k++) {
-        cell[k].style.width = boardMaxSize / rowSize + "px";
-        cell[k].style.height = boardMaxSize / rowSize + "px";
+        cell[k].style.width = boardMaxWidth / rowSize + "px";
+        cell[k].style.height = boardMaxHeight / rowSize + "px";
     }
     
 }
-
 
 // get reference to our squares using query selector
 let squares = document.querySelectorAll('.white-square');
@@ -34,3 +34,49 @@ squares.forEach(square => {
     square.addEventListener('mouseover', () => square.classList.add('black-square'));
     square.addEventListener('click', () => square.classList.remove('black-square'));
 });
+
+// listen for click event on our button
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+
+    let input = prompt('How many squares per side?(max 99.)').toString();
+    
+    // remove previus grid
+    let squares = document.querySelectorAll('.white-square');
+    squares.forEach(square => {
+        square.remove();
+    })
+    let rows = document.querySelectorAll('.row')
+    rows.forEach(row => {
+        row.remove();
+    })
+
+    // create rows
+    let rowSize = input;
+    for(r = 0; r < rowSize; r++) {
+        let row = document.createElement('div');
+        row.classList.toggle('row');
+        board.appendChild(row);
+
+        // create columns
+        const columnSize = rowSize;
+        for(c = 0; c < columnSize; c++) {
+            let column = document.createElement('div');
+            row.appendChild(column);
+            column.classList.toggle('white-square');
+        }
+
+        // dynamically alter the size of squares to fit the dimensions of the board
+        const cell = document.getElementsByClassName('white-square');
+        for(k = 0; k < cell.length; k++) {
+            cell[k].style.width = boardMaxWidth / rowSize + "px";
+            cell[k].style.height = boardMaxHeight / rowSize + "px";
+        }   
+    }
+    squares = document.querySelectorAll('.white-square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => square.classList.add('black-square'));
+        square.addEventListener('click', () => square.classList.remove('black-square'));
+    })
+})
